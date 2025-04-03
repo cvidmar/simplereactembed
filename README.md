fcdb2a4a6bb49176f8ed206eec484b9e73cefb13
Cristian Vidmar <cristian.vidmar@icloud.com>
2025-04-03 15:31:01 +0200

    feat: VITE_BASE_PATH for production

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # simplereactembed
 A simple Go server with a simple React app embedded
+
+## Development
+
+`npm run dev`
+
+## Production
+
+- Set env var VITE_BASE_PATH if you're reverse-proxying to /something/
+- `npm run build`
--- web/vite.config.js
+++ web/vite.config.js
@@ -1,15 +1,19 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: "dist",
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    base: env.VITE_BASE_PATH || "/",
+    plugins: [react()],
+    build: {
+      outDir: "dist",
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, "index.html"),
+        },
       },
     },
-  },
+  };
 });
